666997d3708c01b5620a1db7b0882ad41eeb31a0
Shrirang <deshmukhshrirang01@gmail.com>
2020-10-17 11:45:37 +0530

    Cookies incorporated

--- src/axios-root.js
+++ src/axios-root.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
-  withCredentials: true,
+  baseURL: "http://localhost:3000/api/v1/",
 });
 
 export default instance;
--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
@@ -26,18 +27,13 @@ import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
-import AdminLayout from "layouts/Admin.jsx";
-import UnAuthLayout from "layouts/UnAuthLayout.js";
-import ChatLayout from "layouts/ChatLayout.js";
+import App from "./App.js";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Route path="/auth" render={(props) => <UnAuthLayout {...props} />} />
-      <Route path="/chat" render={(props) => <ChatLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
+  <CookiesProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CookiesProvider>,
   document.getElementById("root")
 );
